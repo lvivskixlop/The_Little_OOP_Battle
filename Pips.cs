cc92ff8205cacea171203aea8b6a18aac54aef8a
lvivskixlop <lvivskixlop1234@gmail.com>
2018-10-24 16:26:30 +0300

    Added Block method to Elf class

--- Pips.cs
+++ Pips.cs
@@ -17,6 +17,7 @@ namespace OOP_C_sharp_5
 	class Elf : Pips
 	{
 		Random rand = new Random();
+
 		public Elf()
 		{
 			PipClass = "None";
@@ -33,9 +34,15 @@ namespace OOP_C_sharp_5
 			return Power;
 		}
 
-		public void Block(int Power)
+		public double Block(double Power)
 		{
-			
+			double RealAttack;
+			double Percent;
+			Percent = rand.Next(1, 13);
+			Percent *= 0.01;
+			RealAttack = Convert.ToDouble(Power) * Percent;
+			Power += RealAttack;
+			return Power;
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,7 @@ namespace OOP_C_sharp_5
 			Elf Legolas=new Elf();
 			Legolas.PipClass = "Archer";
 			Legolas.Health = 486;
-			for (int i = 0; i < 10; i++)
-			{
-				Console.WriteLine(Convert.ToInt32(Legolas.Attack(86)));
-			}
+			
 
 			Elf Elrond = new Elf();
 			Elrond.PipClass = "Warrior";
