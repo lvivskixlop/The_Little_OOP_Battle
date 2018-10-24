d4c1ebeff39902bf7886c6d7d074b6f958d892f4
lvivskixlop <lvivskixlop1234@gmail.com>
2018-10-24 16:13:23 +0300

    Added Attack method to Elf class

---
+++ Pips.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_C_sharp_5
+{
+	
+	abstract class Pips
+	{
+
+		public string PipClass;
+		public int Health;
+		abstract public double Attack(double Power);
+
+	}
+
+	class Elf : Pips
+	{
+		Random rand = new Random();
+		public Elf()
+		{
+			PipClass = "None";
+			Health = 100;
+		}
+		public override double Attack(double Power)
+		{
+			double RealAttack;
+			double Percent;
+			Percent = rand.Next(1, 20);
+			Percent *= 0.01;
+			RealAttack = Convert.ToDouble(Power) * Percent;
+			Power += RealAttack;
+			return Power;
+		}
+
+		public void Block(int Power)
+		{
+			
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP_C_sharp_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Elf Legolas=new Elf();
+			Legolas.PipClass = "Archer";
+			Legolas.Health = 486;
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(Convert.ToInt32(Legolas.Attack(86)));
+			}
+
+			Elf Elrond = new Elf();
+			Elrond.PipClass = "Warrior";
+			Elrond.Health = 672;
+			
+        }
+         
+    }
+}
